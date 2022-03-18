bc23e0a42c365c8377cc81d07c31519cefe075bc
Mairam-dev <diey333@gmail.com>
2022-03-18 01:16:08 +0000

    add file 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,12 @@
+#include "holberton.h"
 #include <stdio.h>
 /**
- * main - prints prime factors of 612852475143
+ * main - prints prime factors of 50829599
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	printf("50829599\n");
 	return (0);
 }
