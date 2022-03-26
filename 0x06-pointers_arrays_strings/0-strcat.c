972cd830ecfea55f55787117d8568f2420af6fed
Mairam-dev <diey333@gmail.com>
2022-03-26 00:37:31 +0000

    add file 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,28 +11,21 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j, k, num;
+	int i;
+	int j;
 
 	i = 0;
 	while (dest[i] != '\0')
+	{
 		i++;
-
+	}
 	j = 0;
 	while (src[j] != '\0')
-		j++;
-
-	if (i < j)
-		num = j;
-	else if (i >= j)
-		num = i;
-
-	for (k = 0; k < num; k++)
 	{
-		if (num == j)
-			dest[k + num - 1] = src[k];
-		else
-			dest[k + num] = src[k];
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-
+	dest[i] = '\0';
 	return (dest);
 }
