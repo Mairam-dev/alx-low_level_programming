97fb4591094d1345346ba2883d52649942c66228
Mairam-dev <diey333@gmail.com>
2022-04-12 06:18:15 +0000

    change content file 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,16 +2,90 @@
 #include <stdlib.h>
 
 /**
- * free_dog - free(memory) dah dog and get fined by animal control
- * @d: pointer to dog stuct in memory.
+ * _strlen - length of string
  *
- * Return: nothing
+ * @s: string
+ * Return: length
  */
-void free_dog(dog_t *d)
+int _strlen(char *s)
 {
-	if (!d)
-		return;
-	free(d->name);
-	free(d->owner);
-	free(d);
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+  * *_strcpy - copy string.
+  *
+  * @src: string src
+  * @dest: string dest
+  *
+  * Return: the pointer to dest
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - create a new dog.
+ *
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
+ *
+ * Return: pointer.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	if (!name)
+		name = "";
+	if (!owner)
+		owner = "";
+
+	ptr = malloc(sizeof(dog_t));
+	if (!ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	ptr->name = malloc(_strlen(name) + 1);
+	if (!ptr->name)
+	{
+		free(ptr->name);
+		return (NULL);
+	}
+
+	ptr->name = _strcpy(ptr->name, name);
+
+	ptr->age = age;
+
+	ptr->owner = malloc(_strlen(owner) + 1);
+	if (!ptr->owner)
+	{
+		free(ptr->name);
+		free(ptr);
+		return (NULL);
+	}
+
+	ptr->owner = _strcpy(ptr->owner, owner);
+
+	return (ptr);
 }
