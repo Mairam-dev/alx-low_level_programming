b949aa8b2c01e6e9cc197fc581c7e0e8e3532662
Mairam-dev <diey333@gmail.com>
2022-04-23 15:28:54 +0000

    add header file

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+void simple_print_buffer(char *buffer, unsigned int size);
+
+char *create_array(unsigned int size, char c);
+
+void print_grid(int **grid, int width, int height);
+
+char *_strdup(char *str);
+
+void print_grid(int **grid, int width, int height);
+
+char *str_concat(char *s1, char *s2);
+
+int **alloc_grid(int width, int height);
+
+void free_grid(int **grid, int height);
+
+char *argstostr(int ac, char **av);
+
+#endif
